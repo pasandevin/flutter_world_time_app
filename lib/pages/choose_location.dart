42f95eea780e84e98d238291c53a1bb2fc54ba4c
pasandevin <jayawardene.pasandevin@gmail.com>
2022-08-24 11:24:01 +0530

    updated location choosing ui

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_world_time_app/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({Key? key}) : super(key: key);
@@ -30,17 +31,46 @@ class _ChooseLocationState extends State<ChooseLocation> {
   //   print('checking if this is called');
   // }
 
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south-korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: Text('ChooseLocation'),
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
         title: Text('Choose a Location'),
         centerTitle: true,
         elevation: 0,
       ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+
       backgroundColor: Colors.grey[200],
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,21 +8,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   Map data = {};
-
 
   @override
   Widget build(BuildContext context) {
-
     data = ModalRoute.of(context)?.settings.arguments as Map;
     print(data);
 
+    String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
+    Color? bgColor = data['isDaytime'] ? Colors.blue : Colors.indigo[700];
 
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+        child: Container(
+          padding: EdgeInsets.fromLTRB(0, 120, 0, 0),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
+            ),
+          ),
           child: Column(
             // mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -31,43 +37,54 @@ class _HomeState extends State<Home> {
                 children: [
                   Container(
                     width: 169.0,
-
                     child: MaterialButton(
                       onPressed: () {
                         Navigator.pushNamed(context, '/location');
                       },
+                      color: Colors.blue,
                       child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                        Text('Choose Location'),
-                        Icon(Icons.arrow_forward),
-                      ]
-                      ),
-                      color: Colors.blue,
+                            Text(
+                              'Choose Location',
+                              style: TextStyle(
+                                color: Colors.grey[300],
+                              ),
+                            ),
+                            Icon(
+                              Icons.edit_location,
+                              color: Colors.grey[300],
+                            ),
+                          ]),
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 20.0,),
+              SizedBox(
+                height: 20.0,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                      data['location'],
+                    data['location'],
                     style: TextStyle(
                       fontSize: 28.0,
                       letterSpacing: 2.0,
+                      color: Colors.white,
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 20.0,),
-              Text(
-                  data['time'],
-              style: TextStyle(
-                  fontSize: 66.0,
-
-              )),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                    color: Colors.white,
+                  ),
+              ),
             ],
           ),
         ),
